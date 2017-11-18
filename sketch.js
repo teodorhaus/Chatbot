2bf97a06e20ba107bdf49a827c1d93d6e1e4c353
teodorhaus <33771886+teodorhaus@users.noreply.github.com>
2017-11-17 22:29:32 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ function setup() {
 
   //start Bot
   bot = new RiveScript();
-  bot.loadFile("brain.rive", brainReady, brainError);
+  bot.loadFile("code/brain.rive", brainReady, brainError);
 
   function brainReady() {
     bot.sortReplies();
